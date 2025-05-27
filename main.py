d03659219672eba1dc3a8ab6ed2becb2b6c0787a
Kevin Mueller <kevin.mueller@tuta.com>
2025-05-27 20:04:43 +0200

    add stats.py with get_num_words func

---
+++ main.py
@@ -0,0 +1,17 @@
+from stats import get_num_words
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    book = get_book_text("./books/frankenstein.txt")
+    word_count = get_num_words(book)
+
+    print(f"{word_count} words found in the document")
+
+
+main()
